25d0488a602222a37e0272d2e774c70a6a9e8cf3
Heng_Zhang <hzhan274@uottawa.ca>
2026-04-27 21:56:28 -0400

    feat: Github Action Enabled

--- eraleap-website/vite.config.ts
+++ eraleap-website/vite.config.ts
@@ -3,5 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/eraleap-website/', // Replace with your repository name
   plugins: [vue()],
 })
+
